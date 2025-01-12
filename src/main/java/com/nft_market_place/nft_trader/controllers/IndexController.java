8be05db1433c2761a2b3bdd40c74dd26861da34a
Md Masud karim <msmasud578@gmail.com>
2025-01-12 10:15:37 +0100

    Added frontend

--- src/main/java/com/nft_market_place/nft_trader/controllers/IndexController.java
+++ src/main/java/com/nft_market_place/nft_trader/controllers/IndexController.java
@@ -3,6 +3,8 @@ package com.nft_market_place.nft_trader.controllers;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+// https://stackoverflow.com/questions/64058885/how-to-integrate-a-react-webapp-inside-a-spring-boot-application-with-jar-packag
+
 @RestController
 public class IndexController {
     @GetMapping("/")
