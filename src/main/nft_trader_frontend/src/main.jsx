fcd6997ed97bc18cbbd3b31d95977e4d967f4c2d
Md Masud karim <msmasud578@gmail.com>
2025-01-14 19:43:37 +0100

    updated the minor stuff

--- src/main/nft_trader_frontend/src/main.jsx
+++ src/main/nft_trader_frontend/src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import { Provider } from "./components/ui/provider"
 import App from './App.jsx'
 import { MetaMaskProvider } from '@metamask/sdk-react'
+// module.createRequire(import.meta.url)('dotenv').config()
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
@@ -12,7 +13,7 @@ createRoot(document.getElementById('root')).render(
           name: "Example React Dapp",
           url: window.location.href,
         },
-        infuraAPIKey: process.env.INFURA_API_KEY,
+        infuraAPIKey: process.env.REACT_APP_INFURA_API_KEY,
       }}
     >
       <Provider>
