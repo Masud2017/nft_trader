9baa797f8c7f5e8c8d24277c3a30eab445a760f5
Md Masud karim <msmasud578@gmail.com>
2025-01-15 22:30:44 +0100

    Added unit test for AuthService

---
+++ src/test/java/com/nft_market_place/nft_trader/services/TestAuthService.java
@@ -0,0 +1,23 @@
+package com.nft_market_place.nft_trader.services;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.aspectj.lang.annotation.Before;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class TestAuthService {
+    @Autowired
+    private AuthService authService;
+
+    @Test
+    public void testGetNonce() {
+        Integer actualNonce = Integer.valueOf(this.authService.getNonce("0x33dfdlkj").getMessage());
+        Integer expectedNonce = 925582;
+
+        assertEquals(expectedNonce, actualNonce);
+
+    }
+}
