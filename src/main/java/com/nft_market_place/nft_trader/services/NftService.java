3047a131f4c97cfeaf19be8dccd55f4addf4c55d
Md Masud karim <msmasud578@gmail.com>
2025-02-03 22:42:33 +0100

    Added extra method to the NftService interface

--- src/main/java/com/nft_market_place/nft_trader/services/NftService.java
+++ src/main/java/com/nft_market_place/nft_trader/services/NftService.java
@@ -5,6 +5,7 @@ import come.nft_market_place.nft_trader.models.Nft;
 public interface NftService {
     ResponseEntity<List<Nft>> getNfts(String address);
     ResponseEntity<Nft> getNft(Long id);
+    ResponseEntity<Nft> getNft(String address);
     ResponseEntity<Nft> addNft(Nft nft);
     ResponseEntity<Nft> updateNft(Nft nft);
     ResponseEntity<Nft> deleteNft(Long id); // It does not delete anything rather it sends the nft to the trash node.
